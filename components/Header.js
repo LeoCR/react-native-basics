d0754ed76fc12fc4c95706fcbe5c2c6c34495528
LeoCR <leonardoas2007@gmail.com>
2020-07-15 11:46:16 -0600

    Implementing Platform.select()

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,32 @@ import Colors from "../constants/colors";
 import TitleText from "./TitleText"
 const Header=props=>{
     return(
-        <View style={styles.header}>
+        <View style={{...styles.headerBase,
+            ...Platform.select({
+                ios:styles.headerIOS,
+                android:styles.headerAndroid
+            })
+        }}>
            <TitleText>{props.title}</TitleText> 
         </View>
     )
 }
 const styles=StyleSheet.create({
-    header:{
+    headerBase:{
         width:"100%",
         height:90,
         paddingTop:36,
-        backgroundColor:Platform.OS==='android'?Colors.primary:Colors.accent,
         alignItems:'center',
         justifyContent:'center',
-        borderBottomColor:Platform.OS==='ios'?'#000000':'#ccc',
+    },
+    headerIOS:{
+        backgroundColor:Colors.accent,
+        borderBottomColor:'#000000',
+        borderBottomWidth:1
+    },
+    headerAndroid:{
+        backgroundColor:Colors.primary,
+        borderBottomColor:'#ccc',
         borderBottomWidth:2
     }
 });
